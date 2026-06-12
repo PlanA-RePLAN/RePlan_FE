e40669a68127e36da85945c2199d93cb57454394
kimseongyeon <seongyeon0810@naver.com>
2026-06-12 16:57:04 +0900

    fix: ChevronLeftIcon 원래대로 수정

--- src/icons/ChevronLeftIcon.tsx
+++ src/icons/ChevronLeftIcon.tsx
@@ -1,7 +1,11 @@
-export default function ChecvronRightIcon() {
+interface ChevronLeftIconProps extends React.SVGProps<SVGSVGElement> {
+  color?: string
+}
+
+export default function ChecvronLeftIcon({ color = '#65696F', width = 24, height = 24, ...props }: ChevronLeftIconProps) {
   return (
-   <svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M8 15L13 10L8 5" stroke="#A9AFB9" stroke-linecap="square"/>
-  </svg>
+    <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M16 4L8 11.7714L16 20" stroke={color} strokeWidth="2" strokeLinecap="square"/>
+    </svg>
   )
 }
