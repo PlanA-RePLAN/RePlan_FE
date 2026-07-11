ed8b98153269fe3406b058433e46e572a2222550
kimseongyeon <seongyeon0810@naver.com>
2026-07-11 13:44:19 +0900

    fix: 완료 투두 토글 모션 수
    정

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -524,8 +524,8 @@ export default function Home() {
                       className={cn(
                         'w-5 h-5 transition-transform duration-300 cursor-pointer',
                         todoHook.isCompletedOpen
-                          ? 'rotate-[180deg]'
-                          : 'rotate-90',
+                          ? 'rotate-[270deg]'
+                          : 'rotate-180',
                       )}
                     />
                   </div>
