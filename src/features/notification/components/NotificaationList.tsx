7e0c84c4eb4b3c6ea238d087a3df934d529d466f
kimseongyeon <seongyeon0810@naver.com>
2026-07-13 00:03:59 +0900

    fix: NotificationList 스타일 수정

--- src/features/notification/components/NotificaationList.tsx
+++ src/features/notification/components/NotificaationList.tsx
@@ -13,14 +13,14 @@ export default function NotificaationList({
     icon, title, content, notificationTime, isRead, onClick
 }:NotificaationListProps) {
   return (
-    <div className="flex items-start gap-3 pt-3" onClick={onClick}>
+    <div className="flex items-start gap-3 pt-3 cursor-pointer " onClick={onClick}>
         <div className="relative">
             {icon}
             { !isRead && (
             <div className="absolute top-0 right-0  bg-danger w-1 h-1 rounded-full"></div>
         )}
         </div>
-        <div className="flex flex-col gap-0.5 pb-3 border-b border-bluegray-light-hover">
+        <div className="w-full flex flex-col gap-0.5 pb-3 border-b border-bluegray-light-hover">
             <h3 className="font-bold text-[14px]">{title}</h3>
             <p className="text-bluegray-dark text-[14px]">{content}</p>
             <p className=" text-bluegray-normal text-[12px]">{notificationTime}</p>
